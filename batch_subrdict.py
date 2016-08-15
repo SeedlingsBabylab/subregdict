1f21ffb99f444727321f78fcccd267f7b63e4dfd
Andrei Amatuni <andrei.amatuni@gmail.com>
2016-08-15 16:56:23 -0400

    work on batch subregion dictionary pipeline

--- batch_subrdict.py
+++ batch_subrdict.py
@@ -7,20 +7,51 @@ class FileGroup:
         self.cha_file = cha
         self.silence_file = silences
 
-def collect_files(dir):
+# def collect_files(dir):
+
+def read_subr_dict(path):
+    subreg_dict = {}
+
+    with open(path, "rU") as input:
+        reader = csv.reader(input)
+        reader.next()
+        prev_file = ""
+        for row in reader:
+            if row[0] != prev_file:
+                subreg_dict[row[0]] = {}
+
+                # make sure it's a legit entry,
+                # with all critical values in place
+                if row[1] and row[2] and row[3] and row[4]:
+                    if row[3] == "onset":
+                        subreg_dict[row[0]][row[1]] = {}
+                        subreg_dict[row[0]][row[1]]["onset"] = row[4]
+                        subreg_dict[row[0]][row[1]]["reg_num"] = row[1]
+                        subreg_dict[row[0]][row[1]]["total_reg_num"] = row[2]
+                        if row[5]:
+                            subreg_dict[row[0]][row[1]]["rank"] = row[5]
+                    if row[3] == "offset":
+                        subreg_dict[row[0]][row[1]]["offset"] = row[4]
+
+            prev_file = row[0]
 
 
 
 if __name__ == "__main__":
     start_dir = sys.argv[1]
+    subreg_dict_file = sys.argv[2]
 
-    file_groups = collect_files(start_dir)
+    subregion_dict = read_subr_dict(subreg_dict_file)
 
-    for group in file_groups:
-        command = ["python", "subregdict.py", group.cha_file, group.lena_file, group.silence_file]
+    print "hello"
 
-        command_string = " ".join(command)
-        print command_string
-
-        pipe = sp.Popen(command, stdout=sp.PIPE, bufsize=10 ** 8)
-        out, err = pipe.communicate()
+    # file_groups = collect_files(start_dir)
+    #
+    # for group in file_groups:
+    #     command = ["python", "subregdict.py", group.cha_file, group.lena_file, group.silence_file]
+    #
+    #     command_string = " ".join(command)
+    #     print command_string
+    #
+    #     pipe = sp.Popen(command, stdout=sp.PIPE, bufsize=10 ** 8)
+    #     out, err = pipe.communicate()
---
+++ comments2dict.py
@@ -0,0 +1,46 @@
+import csv
+import sys
+import re
+
+reg_num_regx = re.compile("(\d+) of (\d+)")
+rank_regx = re.compile("ranked (\d+) of (\d+)")
+
+def parse_comments(comments):
+    results = []
+    for row in comments:
+        temp = [None]*6
+        temp[0] = row[0]
+
+        regx_result = reg_num_regx.search(row[3])
+
+        if regx_result:
+            temp[1] = regx_result.group(1)
+            temp[2] = regx_result.group(2)
+
+        if "starts" in row[3]:
+            temp[3] = "onset"
+        if "ends" in row[3]:
+            temp[3] = "offset"
+
+        temp[4] = row[2]
+
+        rank_regx_result = rank_regx.search(row[3])
+        if rank_regx_result:
+            temp[5] = rank_regx_result.group(1)
+
+        results.append(temp)
+    return results
+
+if __name__ == "__main__":
+    comments_file = sys.argv[1]
+    output_file = sys.argv[2]
+
+    with open(comments_file, "rU") as input:
+        reader = csv.reader(input)
+        header = reader.next()
+        parsed_subregions = parse_comments(reader)
+
+        with open(output_file, "wb") as output:
+            writer = csv.writer(output)
+            writer.writerow(["file", "region_num", "total_reg_num", "on_or_off", "timestamp", "rank"])
+            writer.writerows(parsed_subregions)
